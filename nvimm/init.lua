1e4f355bcb8ae88678a89ffa9e12d96e1fc77095
patdel0 <zefiru@protonmail.com>
2023-06-10 00:29:58 +0100

    Add neogit

--- nvimm/init.lua
+++ nvimm/init.lua
@@ -155,6 +155,7 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  { 'TimUntersberger/neogit', dependencies = 'nvim-lua/plenary.nvim' },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
