a2ddb4810dec168d5799723a0fd3f94d8360248a
patdel0 <zefiru@protonmail.com>
2023-06-12 00:58:19 +0100

    Clone repositories to default dir and add to cache

--- scripts/tmux_project_git_clone.sh
+++ scripts/tmux_project_git_clone.sh
@@ -1,24 +1,46 @@
-   #!/bin/bash
+#!/bin/bash
+source $(dirname $0)/tmux_project_cache_manager.sh
 
-   # Check if the repository URL is provided
-   if [ -z "\$1" ]; then
-       echo "Usage: tgc <repository_url>"
-       exit 1
-   fi
+default_directory="$HOME/code/github.com"
+prompt_for_git_url() {
+    echo "Enter the Git repository URL:"
+    read git_url
+}
 
-   # Clone the repository
-   git clone "\$1"
-   repo_name=$(basename "\$1" .git)
+extract_repo_name() {
+    # Extract the repository name from the Git URL
+    repo_name=$(basename $git_url .git)
+}
 
-   # Check if the repository was cloned successfully
-   if [ ! -d "$repo_name" ]; then
-       echo "Error: Failed to clone the repository"
-       exit 1
-   fi
+define_clone_directory() {
+    clone_directory="${default_directory}/${repo_name}"
+}
 
-   # Change to the repository directory
-   cd "$repo_name" || exit
+ensure_clone_directory_exists() {
+    mkdir -p $clone_directory
+}
 
-   # Create a new tmux session and attach to it
-   tmux new-session -s "$repo_name"
+clone_git_repo() {
+    git clone $git_url $clone_directory
+}
 
+add_repo_to_cache() {
+    add_to_cache_file $clone_directory
+}
+
+create_and_switch_tmux_session() {
+    tmux new-session -d -c $clone_directory -s $repo_name 'nvim -c ":Telescope find_file"'
+    tmux switch-client -t $repo_name
+}
+
+main() {
+    prompt_for_git_url
+    extract_repo_name
+    define_clone_directory
+    ensure_clone_directory_exists
+    clone_git_repo
+    add_repo_to_cache
+    create_and_switch_tmux_session
+}
+
+main
