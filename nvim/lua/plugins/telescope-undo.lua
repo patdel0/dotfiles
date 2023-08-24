f765c6ab2c8ddd7032a1613b2428451f04d25664
patdel0 <zefiru@protonmail.com>
2023-08-25 00:50:26 +0100

    Add telescope extension to show undo history

---
+++ nvim/lua/plugins/telescope-undo.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "debugloop/telescope-undo.nvim",
+  },
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        undo = {},
+      },
+    })
+    require("telescope").load_extension("undo")
+    vim.keymap.set("n", "<leader>ut", "<cmd>Telescope undo<cr>")
+  end,
+}
