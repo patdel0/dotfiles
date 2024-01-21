ce5f9eba64d15146be5ad585fc728af94c38080d
patdel0 <zefiru@protonmail.com>
2024-01-21 00:29:17 +0000

    Install Neogit as an alternative to Magit

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  -- Good alternative to emacs magit
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = true,
+}
