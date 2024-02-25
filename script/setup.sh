732c36886240a201585edca2e96efb91fedc8489
patdel0 <zefiru@protonmail.com>
2024-02-25 07:26:42 +0000

    Update setup script to install tgpt

--- script/setup.sh
+++ script/setup.sh
@@ -6,6 +6,7 @@ rm -rf ~/.oh-my-zsh
 (zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended)
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
 
 sudo apt-get update
 sudo apt-get install fzf -y
