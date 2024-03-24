88c4242d02f6498eda21362ed097460a24db9614
patdel0 <zefiru@protonmail.com>
2024-03-24 18:25:24 +0000

    Add Doom emacs config

--- script/setup.sh
+++ script/setup.sh
@@ -12,14 +12,21 @@ sudo apt-get update
 sudo apt-get install fzf -y
 sudo apt-get install ranger -y
 sudo apt-get install lynx -y
+sudo apt-get install emacs -y
+
+# Install Doom Emacs
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom install
 
 # Link config files
 current_directory=$(pwd)
 rm ~/.tmux.conf
 rm ~/.zshrc
 rm ~/.config/nvim
+rm ~/.config/doom
 
 ln -s $current_directory/.tmux.conf ~/.tmux.conf
 ln -s $current_directory/.zshrc ~/.zshrc
 ln -s $current_directory/nvim ~/.config/nvim
+ln -s $current_directory/doom ~/.config/doom
 echo "Linked config files"
