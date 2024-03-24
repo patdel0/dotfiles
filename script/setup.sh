38fc0a45eaa329b6dc7e6735dc9929165f962f98
patdel0 <zefiru@protonmail.com>
2024-03-24 19:58:33 +0000

    Install dependencies first

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Dependencies installation
+sudo apt-get update
+sudo apt-get install curl fzf ranger lynx emacs nvim -y 
+
 # oh-my-zsh
 rm -rf ~/.oh-my-zsh
 (zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended)
@@ -8,11 +11,6 @@ rm -rf ~/.oh-my-zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
 
-sudo apt-get update
-sudo apt-get install fzf -y
-sudo apt-get install ranger -y
-sudo apt-get install lynx -y
-sudo apt-get install emacs -y
 
 # Install Doom Emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
