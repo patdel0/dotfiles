491ce66b542307ee951b3179be8dce93fe7e5a59
patdel0 <zefiru@protonmail.com>
2024-07-06 20:20:28 +0100

    Install neovim on setup

--- script/setup.sh
+++ script/setup.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 # Dependencies installation
 sudo apt-get update
-sudo apt-get install curl fzf ranger nyxt emacs neovim flatpak -y
+sudo apt-get install curl fzf ranger neovim flatpak gcc ripgrep zsh -y
 
 # oh-my-zsh
 rm -rf ~/.oh-my-zsh
@@ -11,11 +9,6 @@ rm -rf ~/.oh-my-zsh
 # tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
-
-
-# Install Doom Emacs
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
 
 # Link config files
 current_directory=$(pwd)
@@ -27,5 +20,4 @@ rm -rf ~/.config/doom
 ln -s $current_directory/.tmux.conf ~/.tmux.conf
 ln -s $current_directory/.zshrc ~/.zshrc
 ln -s $current_directory/nvim ~/.config/nvim
-ln -s $current_directory/doom ~/.config/doom
 echo "Linked config files"
