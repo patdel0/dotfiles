f72eac50cee6060a4d9fe444939399f4195d4500
patdel0 <zefiru@protonmail.com>
2025-10-23 22:23:46 +0100

    Install `counsel-web` for streamlined web searches

--- doom/packages.el
+++ doom/packages.el
@@ -12,7 +12,12 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
+
+;; Music
 (package! eradio)
+
+;; Web search
+(package! counsel-web)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
